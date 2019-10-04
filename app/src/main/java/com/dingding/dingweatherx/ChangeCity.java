28834b569dd33f888bb13e883494a9d85efa3bda
丁丁 <dingding@bxb.tw>
2019-10-04 17:28:57 +0800

    加第2頁

---
+++ app/src/main/java/com/dingding/dingweatherx/ChangeCity.java
@@ -0,0 +1,14 @@
+package com.dingding.dingweatherx;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChangeCity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_city);
+    }
+}
